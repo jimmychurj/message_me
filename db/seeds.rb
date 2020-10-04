a3c517c71de62df1ee9289af9bf4bee5a145b59d
Jimmy Chu <rjchu@uwaterloo.ca>
2020-10-04 15:44:02 -0400

    course

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "aaaaaa", password: "123456")
+User.create(username: "bbbbbb", password: "123456")
+User.create(username: "cccccc", password: "123456")
+User.create(username: "dddddd", password: "123456")
+User.create(username: "eeeeee", password: "123456")
